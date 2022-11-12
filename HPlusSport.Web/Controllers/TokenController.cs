9c55f0cc6aec4a4acec0c22a7e36398331c016b4
Jeremy MacDougall <macdojer@outlook.com>
2022-11-12 14:56:23 -0500

    Add token-based authenication

---
+++ HPlusSport.Web/Controllers/TokenController.cs
@@ -0,0 +1,71 @@
+﻿using HPlusSport.Web.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using static HPlusSport.Web.Areas.Identity.Pages.Account.LoginModel;
+
+namespace HPlusSport.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly UserManager<HPlusSportWebUser> userManager;
+
+        public TokenController(UserManager<HPlusSportWebUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] InputModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.Email);
+            if (user == null || !await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                return Unauthorized();
+            }
+            var authClaims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("V€r¥ $ecret (not!)"));
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(authClaims),
+                Expires = DateTime.Now.AddMinutes(20),
+                SigningCredentials = new SigningCredentials(
+                    key, SecurityAlgorithms.HmacSha512Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token),
+                expires = token.ValidTo
+            });
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Route("products")]
+        public async Task<IActionResult> GetProducts()
+        {
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("https://localhost:7233/products");
+            var data = await response.Content.ReadAsStringAsync();
+
+            return Ok(data);
+        }
+    }
+}
--- HPlusSport.Web/Program.cs
+++ HPlusSport.Web/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using HPlusSport.Web.Data;
 using HPlusSport.Web.Areas.Identity.Data;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("HPlusSportWebContextConnection") ?? throw new InvalidOperationException("Connection string 'HPlusSportWebContextConnection' not found.");
@@ -14,6 +16,23 @@ builder.Services.AddDefaultIdentity<HPlusSportWebUser>(options => options.SignIn
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes("V€r¥ $ecret (not!)"));
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            IssuerSigningKey = key
+        };
+    });
 
 var app = builder.Build();
 
@@ -29,10 +48,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();;
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapRazorPages();
-
+app.MapControllers();
 app.Run();
